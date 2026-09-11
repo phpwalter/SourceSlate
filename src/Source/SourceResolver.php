c634e1d951bd748e22baabb0635ad4846a16f7d3
Walter <phpwalter@torres.ws>
2026-09-11 02:19:56 -0500

    Capture local Git provenance without network access

--- src/Source/SourceResolver.php
+++ src/Source/SourceResolver.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace SourceSlate\Source;
 
+use SourceSlate\Source\Git\GitClient;
 use SourceSlate\Source\Git\GitSourceProvider;
 
 final readonly class SourceResolver
@@ -23,7 +24,16 @@ final readonly class SourceResolver
             throw new \RuntimeException(sprintf('Project root does not exist: %s', $request->source));
         }
 
-        return new SourceWorkspace(root: $root, remote: false);
+        $git = (new LocalGitInspector(new GitClient()))->inspect($root);
+
+        return new SourceWorkspace(
+            root: $root,
+            remote: false,
+            repository: isset($git['repository']) && is_string($git['repository']) ? $git['repository'] : null,
+            resolvedCommit: isset($git['commit']) && is_string($git['commit']) ? $git['commit'] : null,
+            gitState: isset($git['state']) && is_string($git['state']) ? $git['state'] : null,
+            branch: isset($git['branch']) && is_string($git['branch']) ? $git['branch'] : null,
+        );
     }
 
     private function looksLikeGitSource(string $source): bool
