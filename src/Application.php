9a82de3226870645c84480e4e0bdb92bcf3ae2fc
Walter <phpwalter@torres.ws>
2026-09-11 01:54:50 -0500

    Register cache verification and clear commands

--- src/Application.php
+++ src/Application.php
@@ -3,8 +3,8 @@
 /**
  * @file Application.php
  * @path src/Application.php
- * @version 1.1.0
- * @date 2026-09-10
+ * @version 1.2.0
+ * @date 2026-09-11
  * @author Walter Torres
  * @copyright Copyright 2026, Walter Torres.
  * @license Proprietary
@@ -19,17 +19,12 @@ declare(strict_types=1);
 namespace SourceSlate;
 
 use SourceSlate\Command\BuildCommand;
+use SourceSlate\Command\Cache\CacheClearCommand;
 use SourceSlate\Command\Cache\CacheInfoCommand;
 use SourceSlate\Command\Cache\CacheListCommand;
+use SourceSlate\Command\Cache\CacheVerifyCommand;
 use Symfony\Component\Console\Application as SymfonyApplication;
 
-/**
- * Boots the SourceSlate command-line application.
- *
- * The application owns CLI command registration only. Parsing, documentation
- * modeling, rendering, and source mutation remain delegated to their respective
- * subsystems.
- */
 final class Application extends SymfonyApplication
 {
     public function __construct()
@@ -39,6 +34,8 @@ final class Application extends SymfonyApplication
         $this->add(new BuildCommand());
         $this->add(new CacheListCommand());
         $this->add(new CacheInfoCommand());
+        $this->add(new CacheVerifyCommand());
+        $this->add(new CacheClearCommand());
         $this->setDefaultCommand('build', false);
     }
 }
